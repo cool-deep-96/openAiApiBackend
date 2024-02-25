d59e2d3f9b3129aa72819fa2ddd9e8d92345198b
cool-deep-96 <27581kuldeep.2021cse@gmail.com>
2024-02-26 04:25:17 +0530

    cors added

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import  express  from "express";
 import { config }  from "dotenv";
 import OpenAI from 'openai';
+import cors from 'cors';
 config();
 const app = express();
 app.use(express.json())
+app.use(cors())
 
 const myAi = new OpenAI(
     {
@@ -21,15 +23,15 @@ app.post("/ask", async (req, res) => {
       const response = await myAi.chat.completions.create({
         model: "gpt-3.5-turbo",
         messages: [{ role: "user", content: prompt}],
-        max_tokens: 100
-        });
-        
+        max_tokens: 300
+      });
+
       return res.status(200).json({
         success: true,
         message: response.choices[0].message.content
       });
     } catch (error) {
-        return res.status(400).json({
+        return res.status(404).json({
             success: false,
             message: error.message
         })
